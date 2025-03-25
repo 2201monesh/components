f78d1ec5e93fa03f828173e06d44d53ab7fcdcfe
Monesh Goyal <goyalmonesh2201@gmail.com>
2025-03-25 23:40:18 +0530

    Styled filterDropdown in ComboBox

--- src/components/combobox/FilterDropdown.jsx
+++ src/components/combobox/FilterDropdown.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { IoIosSearch } from "react-icons/io";
 
 function FilterDropdown() {
   return (
-    <div className='w-[180px] h-[200px] border mt-1 rounded-[6px] border border-[#27272A] bg-[#09090B]'>
-      
+    <div className='w-[180px] h-[240px] border mt-1 rounded-[6px] border border-[#27272A] bg-[#09090B] flex flex-col'>
+      <div className='w-[100%] h-[40px] border border-[#27272A] rounded-t-[6px] flex items-center px-3'>
+        <IoIosSearch className='mr-2' size={20}/>
+        <input className='w-[80%] outline-none' placeholder='Filter status...' name="" id="" />
+      </div>  
+      <p className='w-[100%] h-[40px] flex items-center px-3 hover:bg-[#27272A]'>Backlog</p>
+      <p className='w-[100%] h-[40px] flex items-center px-3 hover:bg-[#27272A]'>Todo</p>
+      <p className='w-[100%] h-[40px] flex items-center px-3 hover:bg-[#27272A]'>In Progress</p>
+      <p className='w-[100%] h-[40px] flex items-center px-3 hover:bg-[#27272A]'>Done</p>
+      <p className='w-[100%] h-[40px] rounded-b-[6px] flex items-center px-3 hover:bg-[#27272A]'>Canceled</p>
     </div>
   )
 }
