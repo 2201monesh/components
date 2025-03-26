79cfdd0d8564c8aa3141b70839e6abe5f7bd4b70
Monesh Goyal <goyalmonesh2201@gmail.com>
2025-03-26 22:21:55 +0530

    Functionality for ComboBox

--- src/components/combobox/ComboBox.jsx
+++ src/components/combobox/ComboBox.jsx
@@ -4,8 +4,8 @@ import FilterDropdown from './FilterDropdown';
 function ComboBox() {
 
     const [showFilter, setShowFilter] = useState(false);
-      const dropdownRef = useRef(null);
-
+    const [buttonText, setButtonText] = useState("Set Status");
+    const dropdownRef = useRef(null);
 
     const toggleShowFilter = () => {
         setShowFilter((prev) => !prev)
@@ -26,14 +26,18 @@ function ComboBox() {
     };
   }, [showFilter]);
 
+  const handleButtonText = (value) => {
+    setButtonText(value);
+  }
+
   return (
     <div className='w-screen h-screen flex items-center justify-center bg-[#09090B] text-[#E4E4E4]'>
       <div className='w-[40%] h-[60%] border border-[#27272A] rounded-[6px] flex items-center justify-center flex-col relative'>
         <div className="relative" ref={dropdownRef}>
-            <button className='btn position-sticky w-[150px] h-[40px] border border-[#27272A] rounded-[6px] cursor-pointer hover:bg-[#27272A]' onClick={toggleShowFilter}>Set Status</button>
+            <button className='btn position-sticky w-[150px] h-[40px] border border-[#27272A] rounded-[6px] cursor-pointer hover:bg-[#27272A]' onClick={toggleShowFilter}>{buttonText}</button>
             {showFilter && (
             <div className="absolute top-full left-0 mt-1">
-              <FilterDropdown />
+              <FilterDropdown handleButtonText={handleButtonText} />
             </div>
           )}
         </div>
--- src/components/combobox/FilterDropdown.jsx
+++ src/components/combobox/FilterDropdown.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoIosSearch } from "react-icons/io";
 
-function FilterDropdown() {
+function FilterDropdown({handleButtonText}) {
+
+    const statuses = ["Backlog", "Todo", "In Progress", "Done", "Canceled"];
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const filteredStatuses = statuses.filter(status =>
+      status.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
   return (
-    <div className='w-[180px] h-[240px] border mt-1 rounded-[6px] border border-[#27272A] bg-[#09090B] flex flex-col'>
+    <div className='w-[180px] max-h-[200px] overflow-y-auto border mt-1 rounded-[6px] border border-[#27272A] bg-[#09090B] flex flex-col hide-scrollbar'>
       <div className='w-[100%] h-[40px] border border-[#27272A] rounded-t-[6px] flex items-center px-3'>
         <IoIosSearch className='mr-2' size={20}/>
-        <input className='w-[80%] outline-none' placeholder='Filter status...' name="" id="" />
+        <input className='w-[80%] outline-none' placeholder='Filter status...' onChange={(e) => setSearchTerm(e.target.value)} value={searchTerm} />
       </div>  
-      <p className='w-[100%] h-[40px] flex items-center px-3 hover:bg-[#27272A]'>Backlog</p>
-      <p className='w-[100%] h-[40px] flex items-center px-3 hover:bg-[#27272A]'>Todo</p>
-      <p className='w-[100%] h-[40px] flex items-center px-3 hover:bg-[#27272A]'>In Progress</p>
-      <p className='w-[100%] h-[40px] flex items-center px-3 hover:bg-[#27272A]'>Done</p>
-      <p className='w-[100%] h-[40px] rounded-b-[6px] flex items-center px-3 hover:bg-[#27272A]'>Canceled</p>
+
+      {filteredStatuses.length > 0 ? (
+        filteredStatuses.map((status, index) => (
+          <p 
+            key={status} 
+            onClick={() => handleButtonText(status)}
+            className={`w-[100%] h-[40px] flex items-center px-3 hover:bg-[#27272A] 
+              ${index === filteredStatuses.length - 1 ? 'rounded-b-[6px]' : ''}`} 
+          >
+            {status}
+          </p>
+        ))
+      ) : (
+        <p className="w-[100%] h-[40px] flex items-center px-3 text-gray-500">
+          No results found
+        </p>
+      )}
     </div>
   )
 }
