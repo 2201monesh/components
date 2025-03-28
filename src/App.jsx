79b112d03e31567ad1357249469d825c1a1210c8
Monesh Goyal <goyalmonesh2201@gmail.com>
2025-03-28 23:39:30 +0530

    Added base for Twitter Post Bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ComboBox from './components/combobox/ComboBox'
+import TwitterPostBar from './components/twitterPostBar/TwitterPostBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <ComboBox />
+      <TwitterPostBar />
     </>
   )
 }
---
+++ src/components/twitterPostBar/TwitterPostBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TwitterPostBar() {
+  return (
+    <div className='main-container w-screen h-screen flex items-center justify-center'>
+      <div className='w-[40%] h-[35%] bg-white rounded p-6'>
+        <p>I am batman</p>
+      </div>
+    </div>
+  )
+}
+
+export default TwitterPostBar;
