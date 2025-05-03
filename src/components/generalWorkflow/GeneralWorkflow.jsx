47cc45784b3c45746c6c91759b6b73bfb604bd08
Monesh Goyal <goyalmonesh2201@gmail.com>
2025-05-03 17:48:58 +0530

    Workflow bar

--- src/components/generalWorkflow/GeneralWorkflow.jsx
+++ src/components/generalWorkflow/GeneralWorkflow.jsx
@@ -1,18 +1,27 @@
 import React from "react";
+import { IoIosArrowDown } from "react-icons/io";
+import WorkflowBar from "./WorkflowBar";
 
 function GeneralWorkflow() {
   return (
-    <div className="w-[30%] h-[70%] bg-white rounded-lg p-4">
-      <div className="flex items-center justify-between">
+    <div className="w-[30%] h-[70%] bg-white rounded-lg flex flex-col">
+      <div className="flex items-center justify-between border-b border-gray-200 p-4">
         <div className="flex flex-col">
           <p className="font-medium">General Workflow</p>
           <p className="text-sm text-gray-500">
             Overall task management and efficiency
           </p>
         </div>
-        <button className="px-3 py-1 rounded-lg border-2 border-gray-300 cursor-pointer text-sm">
-          Workflow 1
+        <button className="px-3 py-1 rounded-lg border-2 border-gray-300 cursor-pointer text-sm flex items-center justify-between">
+          <span className="mr-2">Workflow 1</span>
+          <IoIosArrowDown />
         </button>
+      </div>
+      <div className="p-4">
+        <WorkflowBar />
+        <WorkflowBar />
+        <WorkflowBar />
+        <WorkflowBar />
       </div>
     </div>
   );
---
+++ src/components/generalWorkflow/WorkflowBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function WorkflowBar() {
+  return (
+    <div className="w-[100%] border border-gray-300 rounded-lg px-2 py-1 mb-3">
+      workflow bar
+    </div>
+  );
+}
+
+export default WorkflowBar;
