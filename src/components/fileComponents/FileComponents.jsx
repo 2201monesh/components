6a368782ab2a5afd355028c0e6a4b00bb8721c1c
Monesh Goyal <goyalmonesh2201@gmail.com>
2025-05-19 22:19:50 +0530

    changes for file components

--- src/components/fileComponents/FileComponents.jsx
+++ src/components/fileComponents/FileComponents.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Filter } from "lucide-react";
 
 function FileComponents() {
-  return <div>file components</div>;
+  return (
+    <div className="w-[30%] h-[70%] border flex justify-center p-6">
+      <div className="w-[70%] h-8 border rounded-md px-3 py-2 outline-none border-gray-400 flex items-center">
+        <Filter className="text-gray-500 mr-2" size={18} />
+        <input
+          type="text"
+          className=" border-amber-500 px-1 py-1 outline-none"
+          placeholder="filter items..."
+        />
+      </div>
+    </div>
+  );
 }
 
 export default FileComponents;
