2cec686a11cf708097c84b2d99aa7ad200667630
Monesh Goyal <goyalmonesh2201@gmail.com>
2025-05-20 20:15:49 +0530

    Basic structure added in tree node

--- src/components/fileComponents/FileComponents.jsx
+++ src/components/fileComponents/FileComponents.jsx
@@ -30,18 +30,20 @@ function FileComponents() {
 
   return (
     <div className="w-[30%] h-[70%] border flex items-center flex-col p-6">
-      <div className="w-[70%] h-8 border rounded-md px-3 py-2 outline-none border-gray-400 flex items-center">
-        <Filter className="text-gray-500 mr-2" size={18} />
-        <input
-          type="text"
-          className=" border-amber-500 px-1 py-1 outline-none"
-          placeholder="filter items..."
-        />
-      </div>
-      <div>
-        {fileStructure.map((item, index) => (
-          <TreeNode key={index} node={item} />
-        ))}
+      <div className="w-[70%] flex flex-col h-[100%]">
+        <div className="w-[100%] h-8 border rounded-md px-3 py-2 outline-none border-gray-400 flex items-center mb-4">
+          <Filter className="text-gray-500 mr-2" size={18} />
+          <input
+            type="text"
+            className=" border-amber-500 px-1 py-1 outline-none"
+            placeholder="filter items..."
+          />
+        </div>
+        <div className="cursor-pointer">
+          {fileStructure.map((item, index) => (
+            <TreeNode key={index} node={item} />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/fileComponents/TreeNode.jsx
+++ src/components/fileComponents/TreeNode.jsx
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { Folder, FolderOpen, FileText } from "lucide-react";
+import { LuFolderOpen } from "react-icons/lu";
+import { CiFolderOn } from "react-icons/ci";
+import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
 
-function TreeNode({ node }) {
+function TreeNode({ node, level = 0 }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const paddingLeft = `${level * 16}px`;
+
+  if (node.type == "file") {
+    return (
+      <div
+        className="flex items-center gap-2 mb-1 text-gray-800 hover:bg-gray-100 rounded-md py-1 px-2"
+        style={{ paddingLeft }}
+      >
+        <p className="text-md">{node.name}</p>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <p className="text-black">{node.name}</p>
+    <div className="mb-1">
+      <div
+        className="flex items-center hover:bg-gray-100 rounded-md py-1 px-2"
+        style={{ paddingLeft }}
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <span className="mr-1">
+          {isOpen ? (
+            <IoIosArrowDown className="text-gray-600" size={20} />
+          ) : (
+            <IoIosArrowForward className="text-gray-600" size={20} />
+          )}
+        </span>
+        <span className="mr-2">
+          {isOpen ? (
+            <LuFolderOpen className="text-gray-600" size={20} />
+          ) : (
+            <CiFolderOn className="text-gray-600" size={20} />
+          )}
+        </span>
+        <p>{node.name}</p>
+      </div>
+      {isOpen && (
+        <div>
+          {node.children.map((child, index) => (
+            <TreeNode key={index} node={child} level={level + 1} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
